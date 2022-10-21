c8840b4dfe811bb7dd555cff1488c368569deef0
Dan Quenaz <danquenazz@gmail.com>
2022-10-21 11:57:39 -0300

    modified:   config/db.js

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mysql = require("mysql");
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '',
+    password: 'senha123',
     database: 'DB_SPENDING_TOGETHER',
     multipleStatements: true
 });
